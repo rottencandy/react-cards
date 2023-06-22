6a9b384d18866a81a2d34ffcf6c333ee1d3c0aa5
Mohammed Saud <md.saud020@gmail.com>
2023-06-22 14:56:26 +0530

    fix deck positioning

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import { CARD_DATA } from "../lib/card-data"
 import Card from "./Card"
+import './Deck.css'
 
 const Deck = () => {
+  const [cards] = useState(
+    CARD_DATA.map(c => ({ ...c })).reverse()
+  )
+
   return (
     <>
-      {CARD_DATA.map((c, i) => (
-        <Card card={c} idx={i} flipped />
+      {cards.map((c, i) => (
+        <Card key={c.id} card={c} idx={i} flipped />
       ))}
     </>
   )
