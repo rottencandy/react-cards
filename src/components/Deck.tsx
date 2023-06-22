c60d5c2d9b02aabd38c1648fdc8579005abc4e39
Mohammed Saud <md.saud020@gmail.com>
2023-06-22 15:25:51 +0530

    add hide cards button

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import { CARD_DATA } from '../lib/card-data'
+import { sleep } from '../lib/utils'
 import Card from './Card'
 import './Deck.css'
+
+// duration of flip animation for each card during reset (in ms)
+const FLIP_DURATION = 100;
 
 const Deck = () => {
   const [cards, setCards] = useState(
@@ -14,17 +18,34 @@ const Deck = () => {
     setCards([...cards])
   }
 
+  const onHide = async () => {
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i];
+      if (card.flipped) {
+        await sleep(FLIP_DURATION);
+        card.flipped = false;
+        setCards([...cards])
+      }
+    }
+  }
+
   return (
-    <div className="deck">
-      {cards.map((card, i) => (
-        <Card
-          key={card.id}
-          card={card}
-          idx={i}
-          onClick={onCardClick}
-        />
-      ))}
-    </div>
+    <>
+      <div className="deck">
+        {cards.map((card, i) => (
+          <Card
+            key={card.id}
+            card={card}
+            idx={i}
+            onClick={onCardClick}
+          />
+        ))}
+      </div>
+
+      <button onClick={onHide}>
+        Hide
+      </button>
+    </>
   )
 }
 
