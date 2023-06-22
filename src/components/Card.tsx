f19b3f567ac14b407127ae3679e9107d7997e3d0
Mohammed Saud <md.saud020@gmail.com>
2023-06-22 13:44:06 +0530

    avoid using arbitrary constants

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
-import { CardType, CardColor } from '../lib/card-data';
+import { CardType, CardColor, CARD_DATA } from '../lib/card-data';
 import './Card.css'
 
-const Card = ({ card, idx }: { card: CardType, idx: number, flipped?: boolean }) => {
-  const [flip, setFlip] = useState(false);
-  const flipped = flip ? 'flipped' : '';
-  const color = card.color === CardColor.Black ? 'black' : 'red';
+const MAX = CARD_DATA.length;
+
+type CardProps = {
+  card: CardType,
+  idx: number,
+  flipped?: boolean 
+}
+
+const Card = ({ card, idx }: CardProps) => {
+  const [flip, setFlip] = useState(false)
+  const flipped = flip ? 'flipped' : ''
+  const color = card.color === CardColor.Black ? 'black' : 'red'
 
   return (
-    <div className={`card ${flipped}`} style={{ zIndex: flip ? 52 - idx : idx }} onClick={() => setFlip(x => !x)}>
+    <div
+      className={`card ${flipped}`}
+      style={{ zIndex: flip ? MAX - idx : idx }}
+      onClick={() => setFlip(x => !x)}
+    >
       <div className={`front ${color} ${flipped}`}>
         {card.code}
       </div>
